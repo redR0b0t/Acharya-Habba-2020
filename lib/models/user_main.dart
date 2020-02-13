1ea573ca591502873923729c6acf341cdbc3273a
redr0b0t <jatinyadav5511@gmail.com>
2020-02-13 20:04:12 +0530

    completed login functionality

---
+++ lib/models/user_main.dart
@@ -0,0 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  String Name;
+  String Mail;
+  int type;
+  String WhatsApp;
+
+  String College;
+
+
+  ///type specifies account type
+  /// 0----> Volunteer
+  /// 1-----> Students of acharya
+  /// 2------> Student outside of acharya
+
+  UserModel(
+      {
+        this.Name = 'Md imran',
+        this.Mail = '',
+        this.type=0,
+        this.WhatsApp='',
+
+        this.College = 'Acharya Institute of technology',
+      });
+
+  factory UserModel.fromFirestore(DocumentSnapshot data) {
+    return UserModel(
+
+
+        Name: data['name'] ?? 'name',
+        Mail: data['mail'],
+        type: data['type'],
+        College: data['college'] ?? 'college'
+    );
+
+  }
+
+  void setData(DocumentSnapshot doc) {
+    this.Name = doc.data['name'];
+
+    this.Mail = doc.data['mail'];
+    this.type =doc.data['type'];
+    this.College = doc.data['college'];
+
+  }
+
+  Map<String, dynamic> getMap() {
+    return {
+      'name': Name,
+      'mail': Mail,
+      'wapp': WhatsApp,
+
+      'college': College,
+
+      'type': type,
+
+    };
+  }
+}
