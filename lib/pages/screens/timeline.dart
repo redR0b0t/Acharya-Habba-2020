c4b7995c2f8472f4abcb255c368f9cf27e7b2eb8
Imran <imranhussain08209@gmail.com>
2020-02-16 00:04:07 +0530

    improved timecard

--- lib/pages/screens/timeline.dart
+++ lib/pages/screens/timeline.dart
@@ -1,15 +1,11 @@
-import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_flux/flutter_flux.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:habba20/utils/date_time_helper.dart';
 import 'package:habba20/widgets/empty_card.dart';
-
-import 'package:url_launcher/url_launcher.dart';
 import 'package:habba20/widgets/event_card.dart';
+import 'package:habba20/widgets/timeline_card.dart';
 
 class ScaffoldKeyChain {
   static final List<GlobalKey<ScaffoldState>> scaffoldKeyList = [
@@ -26,17 +22,14 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline>
-    with
-        StoreWatcherMixin<Timeline>,
-        SingleTickerProviderStateMixin<Timeline> {
-
+    with StoreWatcherMixin<Timeline>, SingleTickerProviderStateMixin<Timeline> {
   TextStyle _titleStyle = TextStyle(color: Colors.black, fontSize: 30);
   TextStyle _textStyle =
-  TextStyle(color: Colors.black54, fontWeight: FontWeight.w100);
+      TextStyle(color: Colors.black54, fontWeight: FontWeight.w100);
   bool isRegistering = false;
 
   String titleString = 'Acharya Habba';
-  Timestamp d1,d2,d3;
+  Timestamp d1, d2, d3;
 
   @override
   void initState() {
@@ -46,13 +39,10 @@ class _TimelineState extends State<Timeline>
     tabController = TabController(length: 4, vsync: this);
     tabController.addListener(() async {
       QuerySnapshot transactions =
-      await Firestore.instance.collection('days').getDocuments();
-
+          await Firestore.instance.collection('days').getDocuments();
 
       DocumentSnapshot docSnap = transactions.documents[0];
       fetch_day();
-
-
 
       setState(() {
         switch (tabController.index) {
@@ -62,39 +52,35 @@ class _TimelineState extends State<Timeline>
           case 1:
             //ts=int.tryParse((docSnap["day1"]).toString();
             //DatetimeHelper date=new DatetimeHelper((ts));
-           // titleString = '${( docSnap["day1"] as Timestamp).toDate().day}-${( docSnap["day1"] as Timestamp).toDate().month}';
-           // titleString= DatetimeHelper(timestamp: (docSnap['day1'] as Timestamp).millisecondsSinceEpoch).getMonthName();
-            titleString = '${( docSnap["day1"] as Timestamp).toDate().day}-${DatetimeHelper(timestamp: (docSnap['day1'] as Timestamp).millisecondsSinceEpoch).getMonthName()}';
+            // titleString = '${( docSnap["day1"] as Timestamp).toDate().day}-${( docSnap["day1"] as Timestamp).toDate().month}';
+            // titleString= DatetimeHelper(timestamp: (docSnap['day1'] as Timestamp).millisecondsSinceEpoch).getMonthName();
+            titleString =
+                '${(docSnap["day1"] as Timestamp).toDate().day}-${DatetimeHelper(timestamp: (docSnap['day1'] as Timestamp).millisecondsSinceEpoch).getMonthName()}';
             break;
           case 2:
-            titleString = '${( docSnap["day2"] as Timestamp).toDate().day}-${DatetimeHelper(timestamp: (docSnap['day2'] as Timestamp).millisecondsSinceEpoch).getMonthName()}';
+            titleString =
+                '${(docSnap["day2"] as Timestamp).toDate().day}-${DatetimeHelper(timestamp: (docSnap['day2'] as Timestamp).millisecondsSinceEpoch).getMonthName()}';
             break;
           case 3:
-            titleString = '${( docSnap["day3"] as Timestamp).toDate().day}-${DatetimeHelper(timestamp: (docSnap['day3'] as Timestamp).millisecondsSinceEpoch).getMonthName()}';
+            titleString =
+                '${(docSnap["day3"] as Timestamp).toDate().day}-${DatetimeHelper(timestamp: (docSnap['day3'] as Timestamp).millisecondsSinceEpoch).getMonthName()}';
             break;
         }
       });
-
-
     });
   }
 
-  void fetch_day() async{
-
+  void fetch_day() async {
     QuerySnapshot transactions =
-    await Firestore.instance.collection('days').getDocuments();
-
+        await Firestore.instance.collection('days').getDocuments();
 
     DocumentSnapshot docSnap = transactions.documents[0];
-    d1=docSnap['day1'] as Timestamp;
-    d2=docSnap['day2'] as Timestamp;
-    d3=docSnap['day3'] as Timestamp;
-    print({d1,d2,d3});
-
-
-
-
+    d1 = docSnap['day1'] as Timestamp;
+    d2 = docSnap['day2'] as Timestamp;
+    d3 = docSnap['day3'] as Timestamp;
+    print({d1, d2, d3});
   }
+
   TabController tabController;
 
   @override
@@ -171,14 +157,12 @@ class _TimelineState extends State<Timeline>
     );
   }
 
-
-  Widget _streamEvents0(){
+  Widget _streamEvents0() {
     return Scaffold(
       appBar: AppBar(
         title: Text("Event List"),
         backgroundColor: Colors.deepOrange,
       ),
-
       body: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -188,25 +172,24 @@ class _TimelineState extends State<Timeline>
             ),
             borderRadius: BorderRadius.only(bottomLeft: Radius.circular(90))),
         child: StreamBuilder(
-            stream: Firestore.instance
-                .collection('events')
-
-                .snapshots(),
+            stream: Firestore.instance.collection('events').snapshots(),
             builder: (context, snap) {
               if (snap.hasData) {
-                return snap.data.documents.length==0?
-                Text("no events to show"): ListView.builder(
-                    itemCount: snap.data.documents.length,
-                    itemBuilder: (context, index) {
-                      DocumentSnapshot docSnap = snap.data.documents[index];
-                      return EventCard(
-                        image: docSnap['img'],
-                        title: docSnap['name'],
-
-                      );
-                    });
-              }
-              else{
+                return snap.data.documents.length == 0
+                    ? Column(
+                  children: <Widget>[
+                    EmptyCard(type: "Events",)
+                  ],
+                )
+                    : ListView.builder(
+                        itemCount: snap.data.documents.length,
+                        itemBuilder: (context, index) {
+                          DocumentSnapshot docSnap = snap.data.documents[index];
+                          return TimelineCard(
+                            docSnap: docSnap,
+                          );
+                        });
+              } else {
                 return Text("");
               }
             }),
@@ -216,7 +199,7 @@ class _TimelineState extends State<Timeline>
     );
   }
 
-  Widget _streamEvents(int d){
+  Widget _streamEvents(int d) {
     return Container(
       decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -226,39 +209,46 @@ class _TimelineState extends State<Timeline>
           ),
           borderRadius: BorderRadius.only(bottomLeft: Radius.circular(90))),
       child: StreamBuilder(
-          stream: d==1? Firestore.instance
-              .collection('events')
-              .where('event_date', isGreaterThan: d1).where('event_date',isLessThan: d2).orderBy('event_date')
-              .snapshots():d==2?Firestore.instance
-              .collection('events')
-              .where('event_date', isGreaterThan: d2).where('event_date',isLessThan: d3).orderBy('event_date')
-              .snapshots():Firestore.instance
-              .collection('events')
-              .where('event_date', isGreaterThan: d1).orderBy('event_date')
-              .snapshots(),
+          stream: d == 1
+              ? Firestore.instance
+                  .collection('events')
+                  .where('event_date', isGreaterThan: d1)
+                  .where('event_date', isLessThan: d2)
+                  .orderBy('event_date')
+                  .snapshots()
+              : d == 2
+                  ? Firestore.instance
+                      .collection('events')
+                      .where('event_date', isGreaterThan: d2)
+                      .where('event_date', isLessThan: d3)
+                      .orderBy('event_date')
+                      .snapshots()
+                  : Firestore.instance
+                      .collection('events')
+                      .where('event_date', isGreaterThan: d3)
+                      .orderBy('event_date')
+                      .snapshots(),
 //            stream: d==1? Firestore.instance
 //                .collection('events')
 //                .where('event_date', isGreaterThan: d1).where('event_date',isLessThan: (d as Timestamp).toDate().add(new Duration(days:1)))
 //                .snapshots(),
           builder: (context, snap) {
             if (snap.hasData) {
-              return snap.data.documents.length==0?
-              Column(
-                children: <Widget>[
-                  EmptyCard(type: "Events",)
-                ],
-              ): ListView.builder(
-                  itemCount: snap.data.documents.length,
-                  itemBuilder: (context, index) {
-                    DocumentSnapshot docSnap = snap.data.documents[index];
-                    return EventCard(
-                      image: docSnap['img'],
-                      title: docSnap['name'],
-
-                    );
-                  });
-            }
-            else{
+              return snap.data.documents.length == 0
+                  ? Column(
+                      children: <Widget>[
+                        EmptyCard(
+                          type: "Events",
+                        )
+                      ],
+                    )
+                  : ListView.builder(
+                      itemCount: snap.data.documents.length,
+                      itemBuilder: (context, index) {
+                        DocumentSnapshot docSnap = snap.data.documents[index];
+                        return TimelineCard(docSnap: docSnap);
+                      });
+            } else {
               return Text("");
             }
           }),
@@ -267,9 +257,7 @@ class _TimelineState extends State<Timeline>
     );
   }
 
-
-
- /* Widget _buildEvents(int keyIndex, List<Event> events) {
+/* Widget _buildEvents(int keyIndex, List<Event> events) {
     return Scaffold(
       backgroundColor: Colors.transparent,
 //      key: ScaffoldKeyChain.scaffoldKeyList[keyIndex],
@@ -281,7 +269,7 @@ class _TimelineState extends State<Timeline>
     );
   }*/
 
- // Widget buildContent(Event event, int keyIndex) {}
+// Widget buildContent(Event event, int keyIndex) {}
 
 //
 
@@ -290,9 +278,7 @@ class _TimelineState extends State<Timeline>
 class EventContent extends StatelessWidget {
   int keyIndex;
   Event event;
-
   EventContent(this.keyIndex, this.event);
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -421,5 +407,4 @@ class EventContent extends StatelessWidget {
       ),
     );
   }
-
 }*/
