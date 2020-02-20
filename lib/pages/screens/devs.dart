2c4b798acadb9a1e14dccbaa8f621fc9abc564dc
redr0b0t <jatinyadav5511@gmail.com>
2020-02-21 00:17:55 +0530

    added dev info

--- lib/pages/screens/devs.dart
+++ lib/pages/screens/devs.dart
@@ -1,8 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:habba20/utils/style_guide.dart';
 import 'package:habba20/widgets/dev_card.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 import 'about_habba.dart';
 
 class Devs extends StatefulWidget {
@@ -20,129 +21,159 @@ class _DevsState extends State<Devs> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            hexToColor('#EC9F05'),
-            hexToColor('#FF4E00'),
-          ],
-        ),
-      ),
-      child: Scaffold(
+   return Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          automaticallyImplyLeading: false,
-          elevation: 0,
-          title: Text(
-            'Developers',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-        ),
-        body: NotificationListener(
-          onNotification: (notification) {
-            if (notification is ScrollUpdateNotification == true) {
-              setState(() {
-                sunPosition -= notification.scrollDelta / 5;
-                sunSize -= notification.scrollDelta / 8;
-                hillOnePosition -= notification.scrollDelta / 4;
-                hillTwoPosition -= notification.scrollDelta / 3;
-                hillThreePosition -= notification.scrollDelta / 2.8;
-                placePosition -= notification.scrollDelta / 1;
-              });
-            }
-            // just to avoid the jittery line
-            return true;
-          },
-          child: Stack(
-            children: <Widget>[
-              ParalaxWidget(
-                left: MediaQuery.of(context).size.width / 2 + 30,
-                position: this.sunPosition,
-                image: SvgPicture.asset(
-                  'assets/svgs/sun.svg',
-                  width: this.sunSize,
-                  color: hexToColor('#f9e6bf'),
-                ),
-              ),
-              ParalaxWidget(
-                left: 0,
-                position: this.hillOnePosition,
-                image: SvgPicture.asset(
-                  'assets/svgs/hill1.svg',
-                  width: MediaQuery.of(context).size.width,
-                ),
-              ),
-              ParalaxWidget(
-                left: 0,
-                position: this.hillTwoPosition,
-                image: SvgPicture.asset(
-                  'assets/svgs/hill2.svg',
-                  width: MediaQuery.of(context).size.width,
-                ),
-              ),
-              ParalaxWidget(
-                left: 0,
-                position: this.hillThreePosition,
-                image: SvgPicture.asset(
-                  'assets/svgs/hill3.svg',
-                  width: MediaQuery.of(context).size.width,
-                ),
-              ),
-              ParalaxWidget(
-                left: 0,
-                position: this.placePosition,
-                image: SvgPicture.asset(
-                  'assets/svgs/place.svg',
-                  width: MediaQuery.of(context).size.width,
-                ),
-              ),
-              ListView(
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
-                children: <Widget>[
-                  Container(
-                    height: 320,
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(20, 30, 20, 0),
-                    color: hexToColor('#64147e'),
-                    child: ListView(
-                      shrinkWrap: true,
-                      physics: ClampingScrollPhysics(),
-                      children: <Widget>[
-                        Divider(
-                          color: Colors.white60,
-                        ),
-                        Container(height: MediaQuery.of(context).size.height,)
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              GridView.count(
-                shrinkWrap: true,
-                crossAxisCount: 2,
-                children: <Widget>[
-                  DevCard(),
-                  DevCard(),
-                ],
+//        appBar: AppBar(
+//          backgroundColor: Colors.transparent,
+//          automaticallyImplyLeading: false,
+//          elevation: 0,
+//          title: Text(
+//            'Developers',
+//            style: TextStyle(fontWeight: FontWeight.bold),
+//          ),
+//          centerTitle: true,
+//        ),
+        body:
 
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+
+              Container(
+//                width: 500,
+//                decoration: BoxDecoration(
+//                    gradient: LinearGradient(
+//                      begin: Alignment.topCenter,
+//                      end: Alignment.bottomCenter,
+//                      colors: [Color(0xFFf45d27), Color(0xFFf5851f)],
+//                    ),
+//                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(90))),
+                child: StreamBuilder(
+                    stream: Firestore.instance
+                        .collection('devs')
+                        .snapshots(),
+                    builder: (context, snap) {
+                      if (snap.hasData) {
+                        return ListView.builder(
+
+                          //scrollDirection: Axis.horizontal,
+                            itemCount: snap.data.documents.length,
+                            itemBuilder: (context, index) {
+                              DocumentSnapshot docSnap = snap.data
+                                  .documents[index];
+                              return DevCard(docSnap: docSnap);
+                            });
+
+                      }else {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    }),
+
+                // color: Colors.deepOrange,
+              ),
+
+//              GridView.count(
+//                shrinkWrap: true,
+//                crossAxisCount: 2,
+//                children: <Widget>[
+//                  DevCard(),
+//                  DevCard(),
+//                ],
+//
+//              )
+
+   );
   }
 }
 
 class DevDetails extends StatelessWidget {
+  DocumentSnapshot docSnap;
+  DevDetails({this.docSnap});
+  List skills;
+  void initState(){
+    //super.initState();
+    skills = List.from(docSnap['skills']);
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Scaffold(
+
+        backgroundColor: Colors.transparent,
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.white
+          ),
+          height: 800,
+          width: 600,
+          child: Stack(
+
+            children: <Widget>[
+              Text("\n\n\n"),
+              SizedBox(height: 10,),
+              Chip(
+                  label:  Text("Name: ${docSnap['name']}"),),
+
+              Chip(label:Text("About: ${docSnap['about']}")),
+              Text("Skills"),
+              Container(
+                
+                child:
+              ListView.builder(
+                  itemCount: docSnap['skills'].length,
+                  itemBuilder:(context, index) {
+
+
+                return Text(docSnap['skills'][index]);
+
+              }
+
+        ),),
+             Chip(label:Text("Whats app: ${docSnap['wapp']}")),
+              Chip(label:Text("linkedin: ${docSnap['linkedin']}"),
+              onDeleted: () async {
+                await launch(
+                    docSnap['linkedin']);
+              },),
+              Visibility(
+               visible: docSnap['fb']!='',
+                child: Chip(label:Text("Facebook: ${docSnap['fb']}"),
+                  onDeleted: () async {
+                    await launch(
+                        docSnap['fb']);
+                  },),
+              ),
+              Visibility(
+                visible: docSnap['insta']!='',
+                child: Chip(label:Text("instagram: ${docSnap['insta']}"),
+                  onDeleted: () async {
+                    await launch(
+                        docSnap['insta']);
+                  },),
+              ),
+
+
+            ],
+          ),
+        ),
+
+
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //return Container();
     /*showDialog(
         context: context,
         builder: (BuildContext context) {
--- lib/widgets/dev_card.dart
+++ lib/widgets/dev_card.dart
@@ -1,70 +1,60 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:habba20/pages/screens/devs.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class DevCard extends StatelessWidget {
+  DocumentSnapshot docSnap ;
+  DevCard({this.docSnap});
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Stack(
-        children: <Widget>[
-          Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-//          color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  blurRadius: 10.0, // has the effect of softening the shadow
-                  spreadRadius: 0.0, // has the effect of extending the shadow
-                  offset: Offset(
-                    0.0, // horizontal, move right 10
-                    5.0, // vertical, move down 10
-                  ),
-                )
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10.0),
-              child: Image.asset(
-                "assets/logo.png",
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
+    return Container(
+      child:InkWell(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context)=> DevDetails(docSnap: docSnap,)));
+    },child:
           Column(
-            mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+
+
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: CachedNetworkImage(
+                    imageUrl: docSnap['img'],
+                  )
+              ),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      child: Text("Dev name"),
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: Text(docSnap['name']),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              Expanded(
-                child: GestureDetector(
-                  onTap: () {
-                     Navigator.push(context, MaterialPageRoute(builder: (context)=> DevDetails()));
-                  },
-                ),
-              )
+
+//              Expanded(
+//                child: GestureDetector(
+//                  onTap: () {
+//                     Navigator.push(context, MaterialPageRoute(builder: (context)=> DevDetails(docSnap: docSnap,)));
+//                  },
+//                ),
+//              )
             ],
           )
-        ],
-      ),
-    );
+        ,
+
+    ));
   }
 
 }
